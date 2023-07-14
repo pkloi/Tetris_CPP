73440e13929abca92b0736ec2aed28aa837c090e
lsmin <lusimin1999@qq.com>
2023-07-15 00:04:23 +0800

    2023/7/14 备份

--- Tetris/Tetris/Block.cpp
+++ Tetris/Tetris/Block.cpp
@@ -3,9 +3,41 @@
 
 Block::Block()
 {
+	int blocks[7][4]={
+		1,3,5,7,//I
+		2,4,5,7,//Z 1型
+		3,5,4,6,//Z 2型
+		3,5,4,7,//T
+		2,3,5,7,//L
+		3,5,7,6,//J
+		2,3,4,5,//田
+	};
+
+	//随机生成一种俄罗斯方块
+
 }
 
 
 Block::~Block()
 {
+}
+
+void Block::drop()
+{
+
+}
+
+void Block::moveLeftRight(int offset)
+{
+
+}
+
+void Block::rotate()
+{
+
+}
+
+void Block::draw(int leftMargin,int topMargin)
+{
+
 }
